5a75c47630914b4740ed952220ff011b78a887e5
Florence-nyokabi <florencenyokabiwangui@gmail.com>
2023-07-14 08:00:49 +0300

    Added and validated sign up activity

---
+++ app/src/main/java/FloHealthApp/com/ActivitySignUp.kt
@@ -0,0 +1,74 @@
+package FloHealthApp.com
+
+import FloHealthApp.com.databinding.ActivityMainBinding
+import FloHealthApp.com.databinding.ActivitySignUpBinding
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class ActivitySignUp : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setContentView(binding.root)
+
+        binding.btnSignUp.setOnClickListener {
+            clearErrors()
+            validateSignUp()
+        }
+    }
+
+    fun validateSignUp(){
+        val firstName = binding.etFirstName.text.toString()
+        val lastName = binding.etLastName.text.toString()
+        val emailAddress = binding.etEmailAddress.text.toString()
+        val createPassword = binding.etPassword.text.toString()
+        val confirmPassword = binding.etConfirmPassword.text.toString()
+
+        var error  = false
+
+        if (firstName.isEmpty()){
+            binding.tilFirstName.error = "Please input first name"
+            error = true
+        }
+        if (lastName.isEmpty()) {
+            binding.tilLastName.error = "Please input last name"
+            error = true
+        }
+        if(emailAddress.isEmpty()){
+            binding.tilEmailAddress.error = "Please input email address"
+            error = true
+        }
+        if (createPassword.isEmpty()){
+            binding.tilPassword.error = "Please create password"
+            error = true
+        }
+        if (confirmPassword.isEmpty()){
+            binding.tilConfirmPassword.error = "Please confirm password created"
+            error = true
+        }
+        if(!error){
+            Toast.makeText(this, "Signed Up Successfully", Toast.LENGTH_SHORT).show()
+            finish()
+            val intent = Intent(this, PersonalInformation::class.java)
+            startActivity(intent)
+        }
+    }
+    fun clearErrors(){
+        binding.tilFirstName.error = null
+        binding.tilLastName.error = null
+        binding.tilEmailAddress.error = null
+        binding.tilPassword.error = null
+        binding.tilConfirmPassword.error = null
+
+    }
+}
--- app/src/main/java/FloHealthApp/com/MainActivity.kt
+++ app/src/main/java/FloHealthApp/com/MainActivity.kt
@@ -1,11 +1,38 @@
 package FloHealthApp.com
 
+import FloHealthApp.com.databinding.ActivityMainBinding
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
-}
+
+    override fun onResume() {
+        super.onResume()
+
+
+            Picasso
+                .get()
+                .load("https://res.cloudinary.com/dyxt6pqtx/image/upload/v1689244598/Flora-splash-image_onp2hx.jpg")
+                .fit()
+                .centerCrop()
+                .into(binding.backgroundImageView)
+
+        binding.btnGetStarted.setOnClickListener {
+            val intent = Intent(this, ActivitySignUp::class.java)
+            startActivity(intent)
+        }
+
+        }
+
+    }
+
